70b9517fc6e1f42aed69d5bab53a70db88ffbb8a
Samhita <samhitapn.96@gmail.com>
2023-06-04 21:54:29 +0530

    Update getGenomes.py

--- potatoGenome_SolyntaTask/potatoGenomeTask/potatoGenome/management/commands/getGenomes.py
+++ potatoGenome_SolyntaTask/potatoGenomeTask/potatoGenome/management/commands/getGenomes.py
@@ -14,29 +14,29 @@ from potatoGenome.getPotatoGenomes import getGenomeData
 
 from datetime import datetime
 
-import logging
+#import logging
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "Fetches the potato whole genomes and their (meta)data posted after 2018"
         
     def handle(self, *args, **kwargs):
         
-        start = datetime.now()
-        logger.info("\n################# RUN getGenomes Command #################\n")
-        logger.info(f"The Run Started at : {start}")
+        #start = datetime.now()
+        #logger.info("\n################# RUN getGenomes Command #################\n")
+        #logger.info(f"The Run Started at : {start}")
         
         getGenomeData()
         
-        end = datetime.now()
-        logger.info(f"The Run Ended at : {end}")
+        #end = datetime.now()
+        #logger.info(f"The Run Ended at : {end}")
         
-        rt = end - start
-        run_time = rt.total_seconds()
+        #rt = end - start
+        #run_time = rt.total_seconds()
         
-        logger.info(f"The Run took : {run_time} seconds to extract the data")
-        logger.info("\n#################\n")
+        #logger.info(f"The Run took : {run_time} seconds to extract the data")
+        #logger.info("\n#################\n")
 
             
             
